9fc772fb706c3e084759bf25aef610297d22ce88
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-02-19 16:57:03 -0800

    Update PDFNet

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "PDFNet",
             url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/PDFNet.xcframework.zip",
-            checksum: "22494f5ef2e5074193e77052de1a02bda4b4541a970b1916427050ee043385f9"
+            checksum: "4c5c12475d7a76885ede965271ec58363be29ffaca38f8342abc175ad08f1a70"
         ),
         .binaryTarget(
             name: "Tools",
