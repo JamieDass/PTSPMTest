2645afb912f863efde6c530ad3aafb882c11a65a
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-02-24 15:52:49 -0800

    Update

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
             url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/PDFNet.xcframework.zip",
             checksum: "482938ff854d915aa3c75650ef2afbf608a8f50b980bf0bc4ea1ae37b6c5dfce"
         ),
-        .binaryTarget(
-            name: "Tools",
-            url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/Tools.xcframework.zip",
-            checksum: "7c8af303ecf914dab3db1a58e12355e0c3332d05ec79954a9321e731695f363a"
-        )
+//        .binaryTarget(
+//            name: "Tools",
+//            url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/Tools.xcframework.zip",
+//            checksum: "7c8af303ecf914dab3db1a58e12355e0c3332d05ec79954a9321e731695f363a"
+//        )
     ]
 )
