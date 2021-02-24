b09d4a991d9cb3cf84f38b7b4ac3ddc2cc522363
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-02-24 15:54:04 -0800

    Update target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "PDFTron.iOS",
-            targets: ["PDFNet","Tools"]),
+            targets: ["PDFNet"]),
     ],
     targets: [
         .binaryTarget(
