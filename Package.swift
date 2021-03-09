c897c54e52025dcf553cd7766116372027d459ba
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-03-08 16:19:30 -0800

    Update Tools

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "Tools",
             url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/Tools.xcframework.zip",
-            checksum: "f2575666487aa43e388dc9ffc0d1fe963c419945c66ae9eede6879e5a63a5d05"
+            checksum: "15800b8a7c697a7c7f163f817bac31efa969cf1e12e1ff7ec875930dcccc3b34"
         )
     ]
 )
