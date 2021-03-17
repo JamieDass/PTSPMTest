4e969a0fb0e88fa7c095e47a69ea24d43d9ecd93
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-03-17 08:53:52 -0700

    Remove Tools from no-tools branch

--- Package.swift
+++ Package.swift
@@ -7,16 +7,12 @@ let package = Package(
     products: [
         .library(
             name: "PDFTron",
-            targets: ["PDFTron", "PDFTronTools"]),
+            targets: ["PDFTron"]),
     ],
     targets: [
         .binaryTarget(
             name: "PDFTron",
             url: "https://www.pdftron.com/downloads/ios/packages/8.1.2/PDFTron.xcframework.zip",
             checksum: "816f3db41046a2f7eb0760983a69c7099827a2f521626343d3f7db904c80a9a0"),
-        .binaryTarget(
-            name: "PDFTronTools",
-            url: "https://www.pdftron.com/downloads/ios/packages/8.1.2/PDFTronTools.xcframework.zip",
-            checksum: "3ecae0b15f2929fcbdcb65223887069526440663c411031dfff2002a3a63b02d"),
     ]
 )
